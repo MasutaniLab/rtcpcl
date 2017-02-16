6186954508ad2f99a1ff659c250721493b30cad2
y-masutani <masutani@isc.osakac.ac.jp>
2017-02-16 13:47:01 +0900

    テスト用のバッチファイルを収めたディレクトリbatを追加．

---
+++ bat/rtcpclpassthrough.bat
@@ -0,0 +1,16 @@
+@echo off
+set name=%~n0
+set build=..\build
+if not defined RTM_VC_CONFIG set RTM_VC_CONFIG=Debug
+set dlls=pc_type\%RTM_VC_CONFIG%\rtcpcl_pointcloud_type.dll rtcpclbase\%RTM_VC_CONFIG%\rtcpclbase.dll
+for %%d in (%dlls%) do (
+  if not exist %%~nxd (
+    if not exist %build%\%%d (
+      echo %build%\%%d not found. Aborting...
+      pause
+      exit
+    )
+    copy %build%\%%d .
+  )
+)
+start "%name%" "%build%\%name%\%RTM_VC_CONFIG%\%name%_standalone.exe"
